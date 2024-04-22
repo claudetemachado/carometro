601e95e93b701db7b55a8448eb2dfdae96e80227
Claudete Machado <54195430879@escola928.edu>
2024-04-22 15:05:12 -0300

    atualiza ai2

--- src/Pages/Matriculas.jsx
+++
@@ -1,9 +0,0 @@
-import NavBar from "../Components/NavBar"
-function Matriculas(){
-    return(
-        <div>
-            <h1>Página de matrículas</h1>
-        </div>
-    )
-}
-export default Matriculas
